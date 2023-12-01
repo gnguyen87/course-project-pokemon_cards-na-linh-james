edb208da10c18004d1c993168893567e9e673367
James Hernandez <jainicho18@gmail.com>
2023-12-01 13:30:36 -0600

    new classes and poke manager

---
+++ src/PokemonCardManager.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+import edu.macalester.graphics.GraphicsGroup;
+
+public class PokemonCardManager {
+    private ArrayList Cards;
+    private GraphicsGroup cardGroup;
+    private ArrayList cardStatus;
+
+
+}
