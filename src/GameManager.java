6d94a8269dfb04dfd6aed487ffb5dfd3762d6a1e
linhngocdieunguyen <144272243+linhngocdieunguyen@users.noreply.github.com>
2023-12-01 13:30:06 -0600

    hii

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,3 +1,22 @@
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.GraphicsText;
+
 public class GameManager {
-    
+    private CanvasWindow canvas;
+    private double tries;
+    private boolean gameStarted;
+    private GraphicsText numberTriesLeft;
+
+    public GameManager(CanvasWindow canvas) {
+        this.canvas = canvas;
+
+    }
+
+    public double getTries() {
+        return tries;
+    }
+
+    public void setTries(double newTries) {
+        this.tries = newTries;
+    }
 }
--- src/PokemonCardsGame.java
+++ src/PokemonCardsGame.java
@@ -1,5 +1,16 @@
+import java.util.Random;
+
+import edu.macalester.graphics.CanvasWindow;
+
 public class PokemonCardsGame {
+    private CanvasWindow canvas;
     public static void main(String[] args) {
+        PokemonCardsGame game = new PokemonCardsGame();
+        
+    }
+
+    public PokemonCardsGame() {
+        canvas = new CanvasWindow("Pokemon Card Puzzle", 1200, 900);
         
     }
 }
