05b7ff413170cde84d65e07914bda02edf0fac73
linhngocdieunguyen <144272243+linhngocdieunguyen@users.noreply.github.com>
2023-12-13 17:39:57 -0600

    add pokemon class javadocs and delete utilities

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -8,11 +8,17 @@ public class Pokemon extends Image implements Comparable<Pokemon> {
         this.name = name;
     }
 
+    /**
+     * @return name of the Pokemon image
+     */
     public String getName() {
         return name;
     }
 
     @Override
+    /**
+     * Compare if the name of this pokemon and the name of the input pokemon are the same.
+     */
     public int compareTo(Pokemon p2) {
         return name.compareTo(p2.getName());
     }
